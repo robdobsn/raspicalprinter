86de646b1a8e8c5075ac06ce9f2f65016a311f29
Rob Dobson <rob@dobson.com>
2014-06-17 22:20:40 +0100

    Added support for simple UPS

--- Python/calprinter.py
+++ Python/calprinter.py
@@ -10,6 +10,8 @@ from calendars import Calendars
 
 ledPin = 18
 buttonPins = [ 24, 23, 22, 25 ]
+powerSensePin = 17
+batteryPowerOnPin = 27
 
 calendars = Calendars()
 
@@ -18,12 +20,16 @@ printer = Adafruit_Thermal("/dev/ttyAMA0", 19200, timeout=5)
 # Use Broadcom pin numbers (not Raspberry Pi pin numbers) for GPIO
 GPIO.setmode(GPIO.BCM)
 
+# Enable battery power
+GPIO.setup(batteryPowerOnPin, GPIO.OUT)
+GPIO.output(batteryPowerOnPin, GPIO.HIGH)
+
 # Enable LED and button (w/pull-up on latter)
 GPIO.setup(ledPin, GPIO.OUT)
 for buttonPin in buttonPins:
     GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-# LED on while working
+# LED on for now
 GPIO.output(ledPin, GPIO.HIGH)
 
 # Processor load is heavy at startup; wait a moment to avoid
@@ -35,7 +41,7 @@ try:
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	s.connect(('8.8.8.8', 0))
 #	printer.print('My IP address is ' + s.getsockname()[0])
-	printer.feed(3)
+#	printer.feed(3)
 except:
 	printer.boldOn()
 	printer.println('Network is unreachable.')
@@ -55,37 +61,42 @@ for buttonPin in buttonPins:
 # Main loop
 while(True):
 
-	t = time.time()
-	for i in range(len(buttonPins)):
-		# Poll current button state and time
-		buttonState = GPIO.input(buttonPins[i])
+    t = time.time()
+    for i in range(len(buttonPins)):
+        # Poll current button state and time
+        buttonState = GPIO.input(buttonPins[i])
 
-		# Has button state changed?
-		if buttonState != prevButtonStates[i]:
-			prevButtonStates[i] = buttonState   # Yes, save new state/time
-			prevTimes[i] = t
-			#print ("Button " + str(i) + " = " + str(buttonState))
-			if buttonState == True:
-				if i == 0:
-					calendars.printCal(printer, "")
-					printer.feed(3)
-				elif i == 1:
-					calendars.printCal(printer, "Grace")
-					printer.feed(3)
-				elif i == 2:
-					calendars.printCal(printer, "Joe")
-					printer.feed(3)
-				elif i == 3:
-					printer.print('My IP address is ' + s.getsockname()[0])
-					printer.feed(3)
+        # Has button state changed?
+        if buttonState != prevButtonStates[i]:
+            prevButtonStates[i] = buttonState   # Yes, save new state/time
+            prevTimes[i] = t
+            #print ("Button " + str(i) + " = " + str(buttonState))
+            if buttonState == True:
+                if i == 0:
+                    calendars.printCal(printer, "")
+                    printer.feed(3)
+                elif i == 1:
+                    calendars.printCal(printer, "Grace")
+                    printer.feed(3)
+                elif i == 2:
+                    calendars.printCal(printer, "Joe")
+                    printer.feed(3)
+                elif i == 3:
+                    printer.print('My IP address is ' + s.getsockname()[0])
+                    printer.feed(3)
 
-
-	# LED blinks while idle, for a brief interval every 2 seconds.
-	# Pin 18 is PWM-capable and a "sleep throb" would be nice, but
-	# the PWM-related library is a hassle for average users to install
-	# right now.  Might return to this later when it's more accessible.
-	if ((int(t) & 1) == 0) and ((t - int(t)) < 0.15):
-		GPIO.output(ledPin, GPIO.HIGH)
-	else:
-		GPIO.output(ledPin, GPIO.LOW)
+    # Check power sense to see if shutdown is required
+    powerSense = GPIO.input(powerSensePin)
+    if powerSense == False:
+        os.system("sudo shutdown -h now")
+        break
+    
+    # LED blinks while idle, for a brief interval every 2 seconds.
+    # Pin 18 is PWM-capable and a "sleep throb" would be nice, but
+    # the PWM-related library is a hassle for average users to install
+    # right now.  Might return to this later when it's more accessible.
+    if ((int(t) & 1) == 0) and ((t - int(t)) < 0.15):
+        GPIO.output(ledPin, GPIO.HIGH)
+    else:
+        GPIO.output(ledPin, GPIO.LOW)
 
