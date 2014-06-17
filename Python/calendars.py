5ae1d2faa579fb1462933458ac7511b6803c85d8
Rob Dobson <rob@dobson.com>
2014-06-17 22:19:53 +0100

    Initial commit

---
+++ Python/calendars.py
@@ -0,0 +1,62 @@
+from __future__ import print_function
+import requests
+from datetime import datetime
+
+class Calendars:
+	
+	def printCal(self, printer, person):
+		try:
+			r = requests.get ('http://macallan:5000/calendars/api/v1.0/cal')
+			calendarevents = r.json()
+		except:
+			printer.println ("Cannot connect to calendar")
+			return
+		
+		lastDate = ""
+		eventsPrinted = 0
+		for event in calendarevents['calEvents']:
+			# date, duration, summary, location, UID
+			eventDate = event['eventDate']
+			eventTime = event['eventTime']
+			eventDatetime = datetime.strptime(eventDate+eventTime, "%Y%m%d%H:%M")
+			duration = event ['duration'] 
+			summary = event ['summary']
+			location = event ['location']
+			if lastDate != eventDate:
+				if lastDate != "":
+					printer.println()
+					break
+				printer.boldOn()
+				printer.print(eventDatetime.strftime("%a") + " " + eventDatetime.strftime("%d %B"))
+				if person == "":
+					printer.println(" Calendar")
+				else:
+					printer.println(" " + person + "'s Calendar")
+				printer.boldOff()
+				printer.println()
+				#calStr += "<b>" + anEvent[0].strftime("%a") + " (" + anEvent[0].strftime("%d %B)") + ")</b><br/>"
+				lastDate = eventDate
+				
+			if person == "" or (summary.lower().find(person.lower()) != -1):
+				strDurTimes = duration.split(":")
+				durStr = (strDurTimes[0] + "day" + ("s" if strDurTimes[0] != "001" else "")) if strDurTimes[0] != "000" else (strDurTimes[1] + ":" + strDurTimes[2])
+				printer.boldOn()
+				printer.print(eventTime)
+				printer.boldOff()
+				printer.print(" (" + durStr + ") ")
+				printer.boldOn()
+				printer.print (summary)
+				printer.boldOff()
+				printer.println (" " + location)
+				eventsPrinted += 1
+#			calStr += anEvent[0].strftime("%H:%M") + " <small>(" + durStr + ")</small> " + summary + " " + locStr + "<br/>"
+#                        print (anEvent)
+#                    print(date)
+
+#		for event in calendarevents['calEvents']:
+#			print( event ['eventDate'] + ' ' + event ['eventTime'] + ' ' + event ['duration'] + ' ' + event ['location'] + ' ' + event ['summary'])
+
+		if eventsPrinted == 0:
+			printer.boldOn()
+			printer.println("Nothing Doing")
+			printer.boldOff()
---
+++ Python/calprinter.py
@@ -0,0 +1,91 @@
+# Calendar Printer
+# Rob Dobson 2013
+# Adapted from Adafruit pi-thermal-printer project
+
+from __future__ import print_function
+import RPi.GPIO as GPIO
+from Adafruit_Thermal import *
+import time, socket
+from calendars import Calendars
+
+ledPin = 18
+buttonPins = [ 24, 23, 22, 25 ]
+
+calendars = Calendars()
+
+printer = Adafruit_Thermal("/dev/ttyAMA0", 19200, timeout=5)
+
+# Use Broadcom pin numbers (not Raspberry Pi pin numbers) for GPIO
+GPIO.setmode(GPIO.BCM)
+
+# Enable LED and button (w/pull-up on latter)
+GPIO.setup(ledPin, GPIO.OUT)
+for buttonPin in buttonPins:
+    GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# LED on while working
+GPIO.output(ledPin, GPIO.HIGH)
+
+# Processor load is heavy at startup; wait a moment to avoid
+# stalling during greeting.
+time.sleep(30)
+
+# Show IP address (if network is available)
+try:
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.connect(('8.8.8.8', 0))
+#	printer.print('My IP address is ' + s.getsockname()[0])
+	printer.feed(3)
+except:
+	printer.boldOn()
+	printer.println('Network is unreachable.')
+	printer.boldOff()
+	printer.print('Connect display and keyboard\n'
+	  'for network troubleshooting.')
+	printer.feed(3)
+	exit(0)
+
+# Poll initial button state and time
+prevButtonStates = []
+prevTimes = []
+for buttonPin in buttonPins:
+    prevButtonStates.append(GPIO.input(buttonPin))
+    prevTimes.append(time.time())
+
+# Main loop
+while(True):
+
+	t = time.time()
+	for i in range(len(buttonPins)):
+		# Poll current button state and time
+		buttonState = GPIO.input(buttonPins[i])
+
+		# Has button state changed?
+		if buttonState != prevButtonStates[i]:
+			prevButtonStates[i] = buttonState   # Yes, save new state/time
+			prevTimes[i] = t
+			#print ("Button " + str(i) + " = " + str(buttonState))
+			if buttonState == True:
+				if i == 0:
+					calendars.printCal(printer, "")
+					printer.feed(3)
+				elif i == 1:
+					calendars.printCal(printer, "Grace")
+					printer.feed(3)
+				elif i == 2:
+					calendars.printCal(printer, "Joe")
+					printer.feed(3)
+				elif i == 3:
+					printer.print('My IP address is ' + s.getsockname()[0])
+					printer.feed(3)
+
+
+	# LED blinks while idle, for a brief interval every 2 seconds.
+	# Pin 18 is PWM-capable and a "sleep throb" would be nice, but
+	# the PWM-related library is a hassle for average users to install
+	# right now.  Might return to this later when it's more accessible.
+	if ((int(t) & 1) == 0) and ((t - int(t)) < 0.15):
+		GPIO.output(ledPin, GPIO.HIGH)
+	else:
+		GPIO.output(ledPin, GPIO.LOW)
+
